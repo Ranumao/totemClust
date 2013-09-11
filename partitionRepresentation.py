4c5ee6221dcf9b07b29159ed76190cacb59e744b
ranumao <pafpif@gmail.com>
2013-09-11 09:23:49 +0200

    ajout de partitionRepresentation
    c'est un fichier utilitaire pour changer le mode de representation d'une partition, entre array et dic principalement

---
+++ partitionRepresentation.py
@@ -0,0 +1,105 @@
+from scipy import zeros
+from pprint import pprint
+
+def createInvertedAuthorIndex(globAuthorIndex):
+    """
+    aut to indice toward authors array (i2aut)
+    """
+    
+    
+    resList = {}#zeros([len(globAuthorIndex)], str)
+    
+    for i in globAuthorIndex:
+        #print str(i) + str(globAuthorIndex[i])
+        resList[int(globAuthorIndex[i])] = i
+        #print str(globAuthorIndex[i])+" "+str(i)+"\n"
+    #pprint(resList)
+    return resList
+
+def autArrayToi2aut(globAuthorIndex):
+    """
+    aut to indice toward authors array (i2aut)
+    """
+    print "empty function !"
+    exit()
+    
+    
+    
+def dicToArray(dic, globAuthorIndex, translator={"IR":3, "Agents":1, "DB":4, "ML":6, "HCI":5,"AI":2}):
+    """
+    Transform ground truth from dic to array using translator
+    """
+    print "dic glob translator"
+    pprint(dic)
+    pprint(globAuthorIndex)
+    pprint(translator)
+    res=zeros([len(globAuthorIndex)],int)
+    for i in globAuthorIndex:
+        res[globAuthorIndex[i]]=translator[dic[i]]
+    return res
+
+def dicToArraySimple(dic, globAuthorIndex):
+    """
+    Transform ground truth from dic to array
+    """
+    res=zeros([len(globAuthorIndex)],int)
+    for i in globAuthorIndex:
+        res[globAuthorIndex[i]]=dic[i]
+    return res
+
+def arrayToDic_b(partitionArray, invertedIndex):
+    """
+    Change partition notation array -> dic
+    
+    """
+    dic={}
+    for i in invertedIndex:
+        dic[invertedIndex[i]]=partitionArray[i]
+    #pprint(dic)
+        
+    return dic
+    
+    
+
+
+def arrayToDic(partitionArray):
+    """
+    Change partition notation array -> dic
+    
+    """
+    dic={}
+    global inv
+    
+    #print "inv"
+    #pprint(inv)
+    
+    for i in inv:
+    
+        #print str(i) 
+        #print "inv "+inv[i]
+        #print " -- "+str( partitionArray[i])
+    
+        dic[inv[i]]=partitionArray[i]
+    #pprint(dic)
+        
+    return dic
+    
+# etirement (et renversement de la mesure de distance du texte en similarite)
+def splineText(inputValueAsDistance, distanceOutput=False):
+    if distanceOutput:
+        return 1-splineText(inputValueAsDistance)
+
+    #if(1-math.pow(inputValueAsDistance,30)==0):
+    #     return 0.000001
+    #else:
+    #     return 1-math.pow(inputValueAsDistance,30)
+    return 1-inputValueAsDistance
+    
+def arrayToDicIndex(authorIndex):
+    authorIndexIndex={}
+    i=0
+    for aut in authorIndex:
+        authorIndexIndex[aut]=i
+        i=i+1
+    return authorIndexIndex
+    
